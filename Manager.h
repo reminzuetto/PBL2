3df4238401642aef155b4288e7323ef917d5d1ca
a519toandnn <78428242+a519toandnn@users.noreply.github.com>
2023-10-03 23:18:37 +0700

    new commit by toan

--- Manager.h
+++ Manager.h
@@ -1,8 +1,19 @@
+#include <iostream>
+#include "Membership.h"
 #pragma once
 class Manager {
 
     private:
-
+        Film dsphim;
+        Membership *ds_membership;
+        Customer *ds_normal;
 
     public:
+        void UpdateData();
+        void AddFilm();
+        void DeleteFilm();
+        void EditFilm();
+        void ListCustomer();
+        void Revenue();
+        void Display();
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,74 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "Manager.h"
-#include "Customer.h"
+
 using namespace std;
-int main()
-{
-    cout << "test";
-    return 0;
+
+void MainMenu() {
+
+    int Select;    
+    while (true) {
+		system("cls");
+		cout << "\n\n\t\t ====== QUAN LY DAT VE XEM PHIM ======";
+		cout << "\n\n\t1. Dang Nhap Voi Vai Tro Quan Ly ";	
+		cout << "\n\n\t2. Dang Nhap Voi Vai Tro Khach Hang";
+		cout << "\n\n\t0. Ket thuc";
+		cout << "\n\n\t\t ================= END ===============";	
+		cout << "\n\n\tMoi ban nhap lua chon: ";
+		cin >> Select;
+        if(Select ==1) {
+
+            int SelectMng;
+            while (true) {
+
+
+                system("cls");
+                cout << "\n\n\t\t ====== Chuc Nang Cua Quan Ly ======";
+                cout << "\n\n\t1. Them Phim";	
+                cout << "\n\n\t2. Xoa Phim";	
+                cout << "\n\n\t3. Chinh Sua Phim";	
+		        cout << "\n\n\t4. Hien Thi Danh Sach Phim Hien Tai";
+                cout << "\n\n\t5. Kiem Tra Doanh Thu  ";
+                cout << "\n\n\t6. Danh Sach Khach Hang Da Mua Ve";
+                cout << "\n\n\t0. Ket Thuc";
+                cout << "\n\n\t\t ================= END ===============";	
+		        cout << "\n\n\tMoi ban nhap lua chon: ";
+                cin >> SelectMng;
+                if (SelectMng == 0) break;
+
+            }
+            
+        } 
+
+        else if(Select==2) {
+
+            int SelectCsm;
+            while (true) {
+
+                system("cls");
+                cout << "\n\n\t\t ====== Chuc Nang Cua Khach Hang ======";
+                cout << "\n\n\t1. Mua Ve";
+                cout << "\n\n\t0. Kiem Tra Ve Da Dat";
+                cout << "\n\n\t\t ================= END ===============";	
+		        cout << "\n\n\tMoi ban nhap lua chon: ";
+                cin >> SelectCsm;
+                if (SelectCsm == 0) break;
+            }
+            
+        }
+        else {
+
+            system("cls");
+            break;
+
+        }
+		
+		
+	}
+}
+
+int main() {
+
+    Manager m;
+    m.UpdateData();
+    MainMenu();
 }
