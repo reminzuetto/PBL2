5781805767d77e2e4caa3fdce794691561ed1dda
Le Minh Duyet <duyetleminh2004@gmail>
2023-12-05 17:47:13 +0700

    lmd

--- Customer.cpp
+++ Customer.cpp
@@ -51,18 +51,45 @@ Trade Customer::getTrade(int i) const
 }
 ostream& operator<<(ostream& os, const Customer& cus)
 {
-    os << "Name: " << cus.Name << endl;
-    os << "Age: " << cus.Age << endl;
-    os << "Phone Number: " << cus.PhoneNumber << endl;
-    os << "Type of Customer: ";
-    if (cus.TypeOfCus == 0)
-        os << "Student" << endl;
+    os << "Ten: " << cus.Name << endl;
+    os << "Tuoi: " << cus.Age << endl;
+    os << "So dien thoai: " << cus.PhoneNumber << endl;
+    os << "Loai khach hang: ";
+    if (cus.TypeOfCus == "1")
+        os << "Khach hang thanh vien" << endl;
     else
-        os << "Adult" << endl;
-    os << "List of Trade: " << endl;
+        os << "Khach hang binh thuong" << endl;
+    os << "Lich su giao dich: " << endl;
     for (int i = 0; i < cus.ListOfTrade.size(); i++)
     {
         os << cus.ListOfTrade[i];
     }
     return os;
+}
+void Input()
+{
+    cout << "Nhap ten khach hang: ";
+    cin >> this->Name;
+    cout << "Nhap tuoi khach hang: ";
+    cin >> this->Age;
+    cout << "Nhap so dien thoai khach hang: ";
+    cin >> this->PhoneNumber;
+    cout << "Nhap loai khach hang: ";
+    cin >> this->TypeOfCus;
+}
+void Output()
+{
+    cout << "Ten: " << this->Name << endl;
+    cout << "Tuoi: " << this->Age << endl;
+    cout << "So dien thoai: " << this->PhoneNumber << endl;
+    cout << "Loai khach hang: ";
+    if (this->TypeOfCus == 1)
+        cout << "Khach hang thanh vien" << endl;
+    else
+        cout << "Khanh hang binh thuong" << endl;
+    cout << "Lich su giao dich: " << endl;
+    for (int i = 0; i < this->ListOfTrade.size(); i++)
+    {
+        cout << this->ListOfTrade[i];
+    }
 }
